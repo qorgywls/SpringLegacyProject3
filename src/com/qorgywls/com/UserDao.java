056eb97bc9e3612ce641de492000b18e33b43155
Hyojin Baek <qorgywls20@gmail.com>
2023-12-12 15:50:06 +0900

    add world method

--- src/com/qorgywls/com/UserDao.java
+++ src/com/qorgywls/com/UserDao.java
@@ -1,11 +1,15 @@
 package com.qorgywls.com;
 
 public class UserDao {
+	public static void world() {
+		System.out.println("world method");
+	}
 	public static void hello() {
 		System.out.println("hello method");
 	}
 	public static void main(String[] args) {
 		System.out.print("iss53");
+		
 
 	}
 
