73ad8afd75a398b3508679eae5727c007d26a59e
Hyojin Baek <qorgywls20@gmail.com>
2023-12-12 15:48:28 +0900

    add hello method

--- src/com/qorgywls/com/UserDao.java
+++ src/com/qorgywls/com/UserDao.java
@@ -1,7 +1,9 @@
 package com.qorgywls.com;
 
 public class UserDao {
-
+	public static void hello() {
+		System.out.println("hello method");
+	}
 	public static void main(String[] args) {
 		System.out.print("iss53");
 
